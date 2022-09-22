bd018579597b79e530a826ba45e7b29916042115
CristaldoYonathan <cristaldoyonathanariel@gmail.com>
2022-09-22 19:44:45 -0300

    Se agregaron las nuevas rutas del panel de administrador - Usuarios - Inicio

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'Easy Rent')
+
+@section('content_header')
+{{--    el siguiente es el titulo tendrá una clase de bootstrap--}}
+    <h1>Listado de usuarios</h1>
+@stop
+
+@section('content')
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
--- routes/admin.php
+++ routes/admin.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\UserController;
 
 //Route::get('admin', function () { si es que no agregamos prefijo al grupo de rutas
 
-Route::get('', [HomeController::class, 'index']);
+Route::get('', [HomeController::class, 'index']) -> name('admin.home');
+
+//ruta de usuarios con nombre admin.users   UserControlller es el que controla la vista dentro de esa clase estaran todas las funcionalodades de ña vista de usuarios
+Route::resource('users', UserController::class) -> names('admin.users'); // esta ruta se va a llamar en el archivo de configuracion de adminlte.php con el nombre admin.users.index
